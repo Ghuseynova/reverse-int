327d85e9e1e0c834a34cff6ea5dc0d379df1b595
Gunel Huseynova <gorellik246@gmail.com>
2020-10-21 15:42:33 +0300

    refactor: rewrite reverse func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const stringNum = n.toString().includes("-")
+        ? n.toString().slice(1)
+        : n.toString();
+    let reversedNum = "";
+
+    for (let i = stringNum.length; i >= 0; i--) {
+        reversedNum += stringNum.charAt(i);
+    }
+
+    return Number(reversedNum);
+};
